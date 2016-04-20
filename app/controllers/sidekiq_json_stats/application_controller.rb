4637e645e4edb1605c612ca461d1311156d8c7dc
Ben McFadden <ben@forgeapps.com>
2016-04-20 14:47:37 -0700

    Don't put the passed token into the log

--- app/controllers/sidekiq_json_stats/application_controller.rb
+++ app/controllers/sidekiq_json_stats/application_controller.rb
@@ -4,7 +4,7 @@ module SidekiqJsonStats
     def validate_token
       Rails.logger.debug "SIDEKIQ_JSON_STATS_TOKEN not specified" unless ENV["SIDEKIQ_JSON_STATS_TOKEN"].present?
       Rails.logger.debug "Perforing SidekiqJsonStats authorization."
-      unless ENV["SIDEKIQ_JSON_STATS_TOKEN"].present? && authenticate_with_http_token{|token, options| Rails.logger.debug(token); ENV["SIDEKIQ_JSON_STATS_TOKEN"] == token}
+      unless ENV["SIDEKIQ_JSON_STATS_TOKEN"].present? && authenticate_with_http_token{|token, options| ENV["SIDEKIQ_JSON_STATS_TOKEN"] == token}
         # We're acting like we don't exist if the token is invalid instead of returning an authorization error.
         raise AbstractController::ActionNotFound
       end
